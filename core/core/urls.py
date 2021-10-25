9af17e64325c2d4a9068d0ff636ebb5e2dcdd7a4
Tomas-Langhi <tomi.langhi.tl@gmail.com>
2021-10-25 17:59:19 -0300

    axios

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/authentication/', include('dj_rest_auth.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('api/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/authentication/registration/', include('dj_rest_auth.registration.urls')),
 ]
--- core/frontend/src/components/Login/Login.js
+++ core/frontend/src/components/Login/Login.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import axios from 'axios'
 
 const Login = () => {
+
+    const handleSubmit = () => {
+        axios.post('/api/authentication/login/', {
+            username: 'admin',
+            email: 'admin@gmail.com',
+            password: 'pepe123'
+          });
+          console.log("asd")
+    }
     return (
         <>
             <main className="form-signin">
                 <div className="d-flex justify-content-center w-100">
                     <form>
                         <h1 className ="h3 mb-3 fw-normal">Please sign in</h1>
-
+                        <div className ="form-floating">
+                        <input type ="username" className ="form-control" id="floatingInput" placeholder="Username"></input>
+                        <label for="floatingInput">Username</label>
+                        </div>
                         <div className ="form-floating">
                         <input type ="email" className ="form-control" id="floatingInput" placeholder="name@example.com"></input>
                         <label for="floatingInput">Email address</label>
@@ -17,11 +30,7 @@ const Login = () => {
                         <label for="floatingPassword">Password</label>
                         </div>
 
-                        <div className ="checkbox mb-3">
-                        <input type ="checkbox" value="remember-me"></input>
-                        </div>
-                        <button className ="w-100 btn btn-lg btn-primary" type ="submit">Sign in</button>
-                        <p className ="mt-5 mb-3 text-muted">© 2017–2021</p>
+                        <button className ="w-100 btn btn-lg btn-primary" type="button" onClick={handleSubmit}>Sign in</button>
                     </form>
                 </div>
             </main>
